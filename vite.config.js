8d56bd35b5fdb3d12c79a0785fb39c3308d868f7
Shubham Sharma <shubhamgetmail@gmail.com>
2024-10-02 15:59:41 +0530

    Update Proxy in vite.config.js
    Use proxy dynamically for local and product

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // ... other config options ...
   server: {
-    proxy: {
+    proxy: process.env.NODE_ENV === 'development' ? {
       '/api': {
         target: 'https://api.coingate.com',
         changeOrigin: true,
         secure: false,
       },
-    },
+    } : {},
   },
   plugins: [react()],
-})
+});
