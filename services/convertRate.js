8e0f361d6e347c4a0947785186f01dd6aa77e08d
Shubham Sharma <shubhamgetmail@gmail.com>
2024-10-02 17:38:15 +0530

    added proxy cors

--- services/convertRate.js
+++ services/convertRate.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL;
+// const API_URL = import.meta.env.VITE_API_URL;
+const PROXY_URL = import.meta.env.VITE_PROXY_URL;
 export const convertRate = async (amount, exchangeCurrency) => {
     try{
-        const response = await axios.get(`${API_URL}/${exchangeCurrency}`, {
+        const response = await axios.get(`${PROXY_URL}?url=https://api.coingate.com/api/v2/rates/merchant/BTC/${exchangeCurrency}`, {
             headers: { accept: 'text/plain' }
         });
         const rate =  response.data;
-        console.log('API Response:', response.data);
         const convertedRate = (rate * amount).toFixed(2);
         return convertedRate;
     } catch(error){
