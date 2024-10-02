5588cdb0977f4a5863be30e46103998d395deb00
Shubham Sharma <shubhamgetmail@gmail.com>
2024-10-02 15:58:31 +0530

    Update API_URL in convertRate.js

--- services/convertRate.js
+++ services/convertRate.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = '/api/v2/rates/merchant/BTC'
+const API_URL = import.meta.env.VITE_API_URL;
 export const convertRate = async (amount, exchangeCurrency) => {
     try{
         const response = await axios.get(`${API_URL}/${exchangeCurrency}`, {
